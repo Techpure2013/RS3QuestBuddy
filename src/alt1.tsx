bb70925d27c2afdfc53e18e6d850a3b38c034594
Tanner Dunham <TLDunham1995@gmail.com>
2023-11-22 17:25:29 -0600

    a

--- src/alt1.tsx
+++ src/alt1.tsx
@@ -4,24 +4,14 @@ import { useState } from "react";
 
 export const AltGuard = () => {
     const [override, setOverride] = useState(false);
-    if (window.alt1 || override) {
-        return (
-            <MantineProvider
-                withNormalizeCSS
-                withGlobalStyles
-                theme={{ colorScheme: "dark" }}
-            >
-                <App />
-            </MantineProvider>
-        );
-    }
+
     return (
-        <div className="App">
-            <h1>ALT1 not found</h1>
-            <p></p>
-            <Button onClick={() => setOverride(true)}>
-                View Website Anyway
-            </Button>
-        </div>
+        <MantineProvider
+            withNormalizeCSS
+            withGlobalStyles
+            theme={{ colorScheme: "dark" }}
+        >
+            <App />
+        </MantineProvider>
     );
 };
