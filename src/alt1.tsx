e0bf7164b7ee4dd7ff0e554b664e4ec91045c295
Tanner Dunham <TLDunham1995@gmail.com>
2023-11-22 17:46:10 -0600

    adding

--- src/alt1.tsx
+++ src/alt1.tsx
@@ -4,14 +4,42 @@ import { useState } from "react";
 
 export const AltGuard = () => {
     const [override, setOverride] = useState(false);
-
+    if (window.alt1 || override) {
+        return (
+            <MantineProvider
+                withNormalizeCSS
+                withGlobalStyles
+                theme={{ colorScheme: "dark" }}
+            >
+                <App />
+            </MantineProvider>
+        );
+    }
     return (
-        <MantineProvider
-            withNormalizeCSS
-            withGlobalStyles
-            theme={{ colorScheme: "dark" }}
-        >
-            <App />
-        </MantineProvider>
+        <div className="App">
+            <h1>ALT1 not found</h1>
+            <p>
+                Click{" "}
+                <a
+                    href={`alt1://addapp/${window.location.protocol}//${
+                        window.location.host
+                    }/${
+                        !window.location.host.includes("localhost")
+                            ? "RS3QuestBuddy/" //Include repo name (this is only for github pages)
+                            : ""
+                    }appconfig${
+                        !window.location.host.includes("localhost")
+                            ? ".prod" //Target prod (this is only for github pages)
+                            : ""
+                    }.json`}
+                >
+                    here
+                </a>{" "}
+                to add this to alt1
+            </p>
+            <Button onClick={() => setOverride(true)}>
+                View Website Anyway
+            </Button>
+        </div>
     );
 };
