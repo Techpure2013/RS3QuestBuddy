65b00126172511a8a4abe4b6439ca295d242db15
Tanner Dunham <TLDunham1995@gmail.com>
2023-11-22 18:11:34 -0600

    Will maybe work

--- src/alt1.tsx
+++ src/alt1.tsx
@@ -1,10 +1,24 @@
+import React, { useState, useEffect } from "react";
 import { Button, MantineProvider } from "@mantine/core";
 import App from "./App";
-import { useState } from "react";
 
 export const AltGuard = () => {
     const [override, setOverride] = useState(false);
-    if (window.alt1 || override) {
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        // Check for ALT1 presence on component mount
+        if (!window.alt1) {
+            setError(true);
+        }
+    }, []);
+
+    const handleAlt1Click = () => {
+        // Handle ALT1 link click
+        window.location.href = `alt1://addapp/${window.location.protocol}//${window.location.host}/appconfig.json`;
+    };
+
+    if (error || override) {
         return (
             <MantineProvider
                 withGlobalStyles
@@ -17,14 +31,13 @@ export const AltGuard = () => {
             </MantineProvider>
         );
     }
+
     return (
         <div className="App">
             <h1>ALT1 not found</h1>
             <p>
                 Click{" "}
-                <a
-                    href={`alt1://addapp/${window.location.protocol}//${window.location.host}/appconfig.json`}
-                >
+                <a href="#" onClick={handleAlt1Click}>
                     here
                 </a>{" "}
                 to add this to alt1
