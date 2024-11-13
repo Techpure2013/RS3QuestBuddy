58a86427867deed43a48807f5700569dea1f65b3
wwortelhjd@gmail.com <wwortelhjd@gmail.com>
2024-11-13 13:50:39 +0100

    Added a setting toggle to open all accordion items by default

--- src/pages/questpage.tsx
+++ src/pages/questpage.tsx
@@ -47,6 +47,11 @@ import ColorCalculator from "../Handlers/POGCalc.tsx";
 import useGridDisclosure from "./useGridModal.tsx";
 import useLunarGridDisclosure from "./useLunarDisclosure.tsx";
 
+// Define constants for local storage keys to avoid typos and ensure consistency
+const LOCAL_STORAGE_KEYS = {
+	expandAllAccordions: 'expandAllAccordions',
+};
+
 const QuestPage: React.FC = () => {
 	// State and variables
 	const qpname = useLocation();
@@ -89,6 +94,17 @@ const QuestPage: React.FC = () => {
 	const handles = useQuestControllerStore();
 	const [skillLevels, setSkillLevels] = useState<string[]>([]);
 	const [completedQuests, setCompleteQuests] = useState<string[] | null>(null);
+	const storedExpandAll = localStorage.getItem(LOCAL_STORAGE_KEYS.expandAllAccordions);
+	const [expandAllAccordions, setExpandAllAccordions] = useState<boolean>(() => {
+		return storedExpandAll !== null ? JSON.parse(storedExpandAll) : false;
+	});
+	const [expanded, setExpanded] = useState<string[]>(() => {
+		const isExpandAll = storedExpandAll !== null ? JSON.parse(storedExpandAll) : false;
+		
+		if (isExpandAll) return ["item-1", "item-2", "item-3", "item-4", "item-5", "item-6", "item-7"];
+		return [];
+	});
+
 	const handleKeyDown = (event: KeyboardEvent) => {
 		if (!isOpenNotes.current) {
 			if (event.key === " ") {
@@ -442,6 +458,7 @@ const QuestPage: React.FC = () => {
 			React.createRef()
 		);
 	}, [details.stepDetails.length]);
+
 	function handleFalse() {
 		isOpenNotes.current = false;
 	}
@@ -562,12 +579,14 @@ const QuestPage: React.FC = () => {
 			{showStepReq && Array.isArray(QuestDetails) ? (
 				<>
 					<Accordion
-						defaultValue=""
+						multiple
+						defaultValue={expanded}
+						onChange={setExpanded}
 						chevron={
 							<Image src={QuestIcon} alt="Quest Icon" width="20px" height="20px" />
 						}
 					>
-						<Accordion.Item key={1} value="Click to Show Quest Requirements">
+						<Accordion.Item key={"item-1"} value="item-1">
 							<Accordion.Control
 								styles={{
 									control: { color: hasLabelColor ? userLabelColor : "" },
@@ -741,7 +760,7 @@ const QuestPage: React.FC = () => {
 								</div>
 							</Accordion.Panel>
 						</Accordion.Item>
-						<Accordion.Item key={2} value="Click to Show Start Point">
+						<Accordion.Item key={"item-2"} value="item-2">
 							<Accordion.Control
 								className="AccordianControl"
 								styles={{
@@ -758,7 +777,7 @@ const QuestPage: React.FC = () => {
 								</div>
 							</Accordion.Panel>
 						</Accordion.Item>
-						<Accordion.Item key={3} value="Members Or Not">
+						<Accordion.Item key={"item-3"} value="item-3">
 							<Accordion.Control
 								className="AccordianControl"
 								styles={{
@@ -775,7 +794,7 @@ const QuestPage: React.FC = () => {
 								</div>
 							</Accordion.Panel>
 						</Accordion.Item>
-						<Accordion.Item key={4} value="Official Length of Quest">
+						<Accordion.Item key={"item-4"} value="item-4">
 							<Accordion.Control
 								className="AccordianControl"
 								styles={{
@@ -792,7 +811,7 @@ const QuestPage: React.FC = () => {
 								</div>
 							</Accordion.Panel>
 						</Accordion.Item>
-						<Accordion.Item key={5} value="Items Required">
+						<Accordion.Item key={"item-5"} value="item-5">
 							<Accordion.Control
 								className="AccordianControl"
 								styles={{
@@ -825,7 +844,7 @@ const QuestPage: React.FC = () => {
 								</div>
 							</Accordion.Panel>
 						</Accordion.Item>
-						<Accordion.Item key={6} value="Recommended">
+						<Accordion.Item key={"item-6"} value="item-6">
 							<Accordion.Control
 								className="AccordianControl"
 								title="Items You Might Need"
@@ -859,7 +878,7 @@ const QuestPage: React.FC = () => {
 								</div>
 							</Accordion.Panel>
 						</Accordion.Item>
-						<Accordion.Item key={7} value="Enemies to Defeat">
+						<Accordion.Item key={"item-7"} value="item-7">
 							<Accordion.Control
 								className="AccordianControl"
 								styles={{
